9a8f1127f7b8b6d7df06b5367ad527f52e97899a
Matiusha Oleksandr <oleksandr@gmail.com>
2020-03-30 23:48:11 +0300

    add lecture_02 & pom.xlm readme.md

---
+++ src/main/java/com/matiusha/lectures/lecture_02/NathanCycling.java
@@ -0,0 +1,10 @@
+package com.matiusha.homeworks;
+
+public class NathanCycling {
+    public static void main(String[] args) {
+       double time = 11.8;
+       double drinksOfWater =0.5;
+       int liters = (int) time*drinksOfWater;
+        System.out.println(liters);
+    }
+}
